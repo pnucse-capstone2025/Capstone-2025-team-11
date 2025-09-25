098d78c61636cfe003fd6bfbccf2b3d75d92d0b4
Noah0615 <101160620+Noah0615@users.noreply.github.com>
2025-09-25 18:01:24 +0900

    Delete code/db.py

--- code/db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_db_connection():
-    return mysql.connector.connect(
-        host='localhost',
-        user='underdog',
-        password='12345',
-        database='beautiAI'
-    )
